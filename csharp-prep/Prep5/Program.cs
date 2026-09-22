981d9e69997ba557fa46e96e80675e495786f8ad
sm644me <lily_cody2000@hotmail.com>
2026-09-22 12:40:38 -0700

    Completed Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,46 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    static int PromptUserBirthYear(out int year)
+    {
+        Console.Write("Please enter your birth year: ");
+        year = int.Parse(Console.ReadLine());
+        return year;
+    }
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+    static void DisplayResult(string name, int number, int year)
+    {
+        Console.WriteLine($"{name}, your squared number is {number}.");
+        year = 2026 - year;
+        Console.WriteLine($"{name}, you will turn {year} this year.");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int year = PromptUserBirthYear(out year);
+        int square = SquareNumber(number);
+        DisplayResult(name, square, year);
     }
 }
