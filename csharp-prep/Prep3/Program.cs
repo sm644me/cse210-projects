55ff4de22398ea32c3a695006301a7497f644370
sm644me <lily_cody2000@hotmail.com>
2026-09-22 09:20:20 -0700

    Completed Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Create a random number.
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guessInt = -1;
+        do
+        {
+            Console.WriteLine("What is your guess? ");
+            string guessString = Console.ReadLine();
+            guessInt = int.Parse(guessString);
+            if (guessInt < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessInt > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+        } while (guessInt != magicNumber);
+        Console.WriteLine("You guessed it!");
     }
 }
